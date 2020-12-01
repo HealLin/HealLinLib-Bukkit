582547d6996886df525a5ea2a13d172fbd62fcab
择忆霖心 <644888090@qq.com>
2020-12-01 22:46:27 +0800

    版本: 2.002 删除了部分的无用类

---
+++ src/main/java/com/corelin/library/plugin/interfaces/PermissionHandle.java
@@ -0,0 +1,12 @@
+package com.corelin.library.plugin.interfaces;
+
+/**
+ * @author 择忆霖心
+ * @简述 TODO
+ * @时间 22:45
+ * @版本 1.0
+ */
+public interface PermissionHandle {
+
+    String run(String permission);
+}
